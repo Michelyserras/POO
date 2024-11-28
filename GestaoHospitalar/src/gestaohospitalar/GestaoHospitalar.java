b44fe7cece58aed18430c1711873f6e90d1a70d5
aluno <aluno@LAB>
2024-11-28 19:16:36 -0300

    testeee

--- GestaoHospitalar/src/gestaohospitalar/GestaoHospitalar.java
+++ GestaoHospitalar/src/gestaohospitalar/GestaoHospitalar.java
@@ -16,6 +16,7 @@ public class GestaoHospitalar {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("teste");
+        System.out.println("teste");
     }
     
 }
