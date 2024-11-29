4609bf1b957a56fb618384d4cf8d3a8ecfd75951
aluno <aluno@LAB>
2024-11-29 20:21:29 -0300

    classes service

---
+++ GestaoHospitalar/src/gestaohospitalar/Service/PacienteService.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestaohospitalar.Service;
+import gestaohospitalar.Dao.PacienteDao;
+public class PacienteService {
+    PacienteDao pacienteDao = new PacienteDao();
+    
+    
+}
