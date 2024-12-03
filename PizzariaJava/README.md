82079889810d3959f3d6a7c06680bdaf0e07303c
Michelyserras <Michelysrrs@gmail.com>
2024-12-03 14:35:10 -0300

    apagando atvPizzaria

--- PizzariaJava/README.md
+++
@@ -1,3 +0,0 @@
-# PizzariaJava
-Alunos: Fernando Antonio e Michely Serras <br>
-![alt text](https://github.com/nandoant/PizzariaJava/blob/main/DiagramaDeClasses%20(1).png?raw=true)
--- PizzariaJava/src/atv_pazi_pizzaria/Model/Pizzas/Caipira.java
+++
@@ -1,9 +0,0 @@
-package atv_pazi_pizzaria.Model.Pizzas;
-
-import atv_pazi_pizzaria.Model.Pizza;
-
-public class Caipira extends Pizza {
-    public Caipira() {
-        super(35.0, "Caipira", "Ingredientes da Caipira", 1);
-    }
-}
