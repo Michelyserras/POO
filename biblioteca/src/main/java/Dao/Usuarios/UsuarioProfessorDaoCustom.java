4ba4499153710e46b666e25faada38e43f6bb70f
Michelyserras <michelysrrs@gmail.com>
2025-01-16 19:32:45 -0300

    add produtoDao

--- biblioteca/src/main/java/Dao/Usuarios/UsuarioProfessorDaoCustom.java
+++
@@ -1,9 +0,0 @@
-package Dao.Usuarios;
-
-import Model.Livro;
-
-public interface UsuarioProfessorDaoCustom {
-    public boolean bloqueiaLivro(Livro it, int prazo);
-    public boolean desbloqueiaLivro(Livro it);
-   
-}
--- loja/src/main/java/com/loja/dao/ProdutoDaoJDBC.java
+++ loja/src/main/java/com/loja/dao/ProdutoDaoJDBC.java
@@ -1,5 +1,41 @@
 package com.loja.dao;
 
-public class ProdutoDaoJDBC {
+import java.util.List;
+
+import com.loja.entities.dto.ItemDto;
+
+public class ProdutoDaoJDBC implements ProdutoDao{
+
+    public void criarTabela(){
+        String query = """
+                CREATE TABLE IF NOT EXISTS produtos (
+                    id BIGINT AUTO_INCREMENT PRIMARY KEY
+                )
+                """;
+    }
+
+    @Override
+    public void adicionarProduto(ProdutoDto item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarProduto'");
+    }
+
+    @Override
+    public void removerItem(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removerItem'");
+    }
+
+    @Override
+    public void atualizarItem(ProdutoDto item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarItem'");
+    }
+
+    @Override
+    public List<ItemDto> listarItens() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarItens'");
+    }
 
 }
